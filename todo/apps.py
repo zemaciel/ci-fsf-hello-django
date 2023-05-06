f83b892051de35b9c6f359a05291ca4af205e6c8
zemaciel <joemacbr@gmail.com>
2023-05-06 12:03:53 +0000

    Apply migration and register models

--- todo/apps.py
+++ todo/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+import os
 
 class TodoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo'
+
+if os.path.exists("env.py"):
+    import env
+
+print(os.getenv("envpy_test"))
