d436c9edb869ef26db99b890b931e4046607a510
zemaciel <joemacbr@gmail.com>
2023-05-13 09:08:07 +0000

    First tests

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestDjango(TestCase):
+
+    def test_this_thing_works1(self):
+        self.assertEqual(1,1)
+    def test_this_thing_works2(self):
+        self.assertEqual(1,1)
+    def test_this_thing_works3(self):
+        self.assertEqual(1,1)
+    def test_this_thing_works4(self):
+        self.assertEqual(1,1)
