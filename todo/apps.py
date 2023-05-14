7eebca4d5c0591b0a1d17ebc57ae28e9eeef1e97
zemaciel <joemacbr@gmail.com>
2023-05-14 08:33:59 +0000

    Try fixing Heroku Error

--- todo/apps.py
+++ todo/apps.py
@@ -1,11 +1,14 @@
 from django.apps import AppConfig
 import os
 
+
 class TodoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo'
 
+
 if os.path.exists("env.py"):
     import env
+
 
 print(os.getenv("envpy_test"))
